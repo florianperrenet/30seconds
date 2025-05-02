a443069a7b01ac0546c0599a7c64522ae2d34089
Joris Perrenet <joris.perrenet@gmail.com>
2025-05-02 18:18:10 +0200

    Updates

---
+++ static/immutable/chunks/paths.D2G2Bojg.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1wrva8v)==null?void 0:s.base)??"/30seconds";var a;const t=((a=globalThis.__sveltekit_1wrva8v)==null?void 0:a.assets)??e;export{t as a,e as b};
