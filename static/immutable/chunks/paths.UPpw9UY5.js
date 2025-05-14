f1b36e7946dd888b47fc51bacac5d5588ef9f624
Florian Perrenet <florian.perrenet@gmail.com>
2025-05-14 21:07:26 +0200

    Updates

--- static/immutable/chunks/paths.UPpw9UY5.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1y1mjua)==null?void 0:s.base)??"/30seconds";var a;const t=((a=globalThis.__sveltekit_1y1mjua)==null?void 0:a.assets)??e;export{t as a,e as b};
